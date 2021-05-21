b5991b9c2e64117e1b0b785a6600a906c88f2646
luizloja <luizloja@gmail.com>
2021-05-21 10:23:44 -0300

    Removed vue_frontend from app
    Relocated vue_frontend to the root of the project. I had some performance issues with it in app

--- app/vue_frontend/webpack.mix.js
+++
@@ -1,7 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.setPublicPath('../../public/vue')
-	.js('src/js/app.js', 'js').vue()
-    .sass('src/sass/app.scss', 'css')
-	.copy('src/index.html', '')
-    .sourceMaps();
