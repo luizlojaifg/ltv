ef8af190c847258f13b320fe69b7842b9ea69bff
luizloja <luizloja@gmail.com>
2021-05-21 00:08:22 -0300

    Frontend first setup and convert to short code implemented
    All Vue Quasar environment configured
    Readme updated
    Code the first interface to send a full url and return the shortcode
    Already pre config the table to show the 100 most accessed shortcodes.

---
+++ app/vue_frontend/webpack.mix.js
@@ -0,0 +1,7 @@
+let mix = require('laravel-mix');
+
+mix.setPublicPath('../../public/vue')
+	.js('src/js/app.js', 'js').vue()
+    .sass('src/sass/app.scss', 'css')
+	.copy('src/index.html', '')
+    .sourceMaps();
